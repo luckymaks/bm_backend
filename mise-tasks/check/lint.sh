2cc565c7e0a0975a04a69ebf39bd5a35171c0eff
Maksym Maksymenko <maksimenko.maksim.2310@gmail.com>
2026-01-20 23:43:31 +0200

    Add MISE task scripts for testing, linting, formatting, and code generation
    Includes:
    - Task scripts under `mise-tasks` for testing, linting, formatting, compilation checks, and code changes validation.
    - `mise.toml` configuration file specifying required tools and versions.
    - Structured organization for project automation tasks.

---
+++ mise-tasks/check/lint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#MISE description="Lint the code base"
+set -euo pipefail
+find mise-tasks -name '*.sh' -type f -print0 | xargs -0 shellcheck
+
+# Backend code.
+cd backend
+golangci-lint config verify
+golangci-lint run ./...
+buf lint
+
+# AWS CDK infra.
+cd -
+cd infra
+golangci-lint run ./...
+
+# terraform.
+cd -
+cd infra/tf
+terraform validate
+
+# cloudformation.
+cd -
+cfn-lint mise-tasks/aws/pre-bootstrap.cfn.yaml
