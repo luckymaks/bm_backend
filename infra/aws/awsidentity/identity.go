f75d620ec64e53a1b3eba5134310be0461068e83
Maksym Maksymenko <maksimenko.maksim.2310@gmail.com>
2026-01-28 23:22:51 +0200

    Enhance AWS Cognito identity management with additional attributes and multi-region support, including domain and resource server configurations.

--- infra/aws/awsidentity/identity.go
+++ infra/aws/awsidentity/identity.go
@@ -9,73 +9,100 @@ import (
 	"github.com/luckymaks/bm_backend/infra/aws/cdk/cdkutil"
 )
 
-const (
-	identityNamespace = "identity"
-	userPoolIDParam   = "user-pool-id"
-)
+const paramsNamespace = "identity"
 
 type IdentityProps struct{}
 
 type Identity interface {
 	UserPool() awscognito.IUserPool
+	UserPoolID() *string
+	MainResourceServer() awscognito.IUserPoolResourceServer
+	CognitoDomain() *string
 }
 
 type identity struct {
-	userPool awscognito.IUserPool
+	userPool           awscognito.IUserPool
+	userPoolID         *string
+	mainResourceServer awscognito.IUserPoolResourceServer
+	cognitoDomain      *string
 }
 
-func New(scope constructs.Construct, _ IdentityProps) Identity {
-	scope, con := constructs.NewConstruct(scope, jsii.String("Identity")), &identity{}
+func New(parent constructs.Construct, _ IdentityProps) Identity {
+	scope, con := constructs.NewConstruct(parent, jsii.String("Identity")), &identity{}
 	qual := cdkutil.QualifierFromContext(scope)
-
-	if cdkutil.IsPrimaryRegion(scope) {
-		pool := awscognito.NewUserPool(scope, jsii.String("UserPool"), &awscognito.UserPoolProps{
-			UserPoolName: jsii.Sprintf("%s-users", qual),
-			SelfSignUpEnabled: jsii.Bool(false),
-			SignInAliases: &awscognito.SignInAliases{
-				Email: jsii.Bool(true),
-			},
-			AutoVerify: &awscognito.AutoVerifiedAttrs{
-				Email: jsii.Bool(true),
-			},
-			StandardAttributes: &awscognito.StandardAttributes{
-				Email: &awscognito.StandardAttribute{
-					Required: jsii.Bool(true),
-					Mutable:  jsii.Bool(true),
-				},
-			},
-			PasswordPolicy: &awscognito.PasswordPolicy{
-				MinLength:        jsii.Number(8),
-				RequireLowercase: jsii.Bool(true),
-				RequireUppercase: jsii.Bool(true),
-				RequireDigits:    jsii.Bool(true),
-				RequireSymbols:   jsii.Bool(true),
-			},
-			AccountRecovery: awscognito.AccountRecovery_EMAIL_ONLY,
-			RemovalPolicy:   awscdk.RemovalPolicy_DESTROY,
-		})
-
-		pool.AddResourceServer(jsii.String("MainResourceServer"), &awscognito.UserPoolResourceServerOptions{
-			Identifier: jsii.String("main"),
-			Scopes: &[]awscognito.ResourceServerScope{
-				awscognito.NewResourceServerScope(&awscognito.ResourceServerScopeProps{
-					ScopeName:        jsii.String("admin"),
-					ScopeDescription: jsii.String("Administrative access"),
-				}),
-			},
-		})
-
-		awsparams.Store(scope, "UserPoolIDParam", identityNamespace, userPoolIDParam, pool.UserPoolId())
-
-		con.userPool = pool
-	} else {
-		userPoolID := awsparams.Lookup(scope, "LookupUserPoolID", identityNamespace, userPoolIDParam, "user-pool-id")
-		con.userPool = awscognito.UserPool_FromUserPoolId(scope, jsii.String("UserPool"), userPoolID)
+	
+	if !cdkutil.IsPrimaryRegion(scope) {
+		con.userPoolID = awsparams.Lookup(scope, "LookupUserPoolID", paramsNamespace, "user-pool-id", "user-pool-id-lookup")
+		
+		return con
 	}
-
+	
+	con.userPool = awscognito.NewUserPool(scope, jsii.String("UserPool"), &awscognito.UserPoolProps{
+		UserPoolName:      jsii.Sprintf("%s-users", qual),
+		SelfSignUpEnabled: jsii.Bool(false),
+		SignInAliases: &awscognito.SignInAliases{
+			Email: jsii.Bool(true),
+		},
+		AutoVerify: &awscognito.AutoVerifiedAttrs{
+			Email: jsii.Bool(true),
+		},
+		StandardAttributes: &awscognito.StandardAttributes{
+			Email: &awscognito.StandardAttribute{
+				Required: jsii.Bool(true),
+				Mutable:  jsii.Bool(true),
+			},
+		},
+		PasswordPolicy: &awscognito.PasswordPolicy{
+			MinLength:        jsii.Number(8),
+			RequireLowercase: jsii.Bool(true),
+			RequireUppercase: jsii.Bool(true),
+			RequireDigits:    jsii.Bool(true),
+			RequireSymbols:   jsii.Bool(true),
+		},
+		AccountRecovery: awscognito.AccountRecovery_EMAIL_ONLY,
+		RemovalPolicy:   awscdk.RemovalPolicy_DESTROY,
+	})
+	
+	con.userPoolID = con.userPool.UserPoolId()
+	
+	userPoolDomain := con.userPool.AddDomain(jsii.String("CognitoDomain"), &awscognito.UserPoolDomainOptions{
+		CognitoDomain: &awscognito.CognitoDomainOptions{
+			DomainPrefix: jsii.Sprintf("%s-auth", qual),
+		},
+		ManagedLoginVersion: awscognito.ManagedLoginVersion_NEWER_MANAGED_LOGIN,
+	})
+	
+	con.cognitoDomain = userPoolDomain.BaseUrl(nil)
+	
+	adminScope := awscognito.NewResourceServerScope(&awscognito.ResourceServerScopeProps{
+		ScopeName:        jsii.String("admin"),
+		ScopeDescription: jsii.String("Administrative access"),
+	})
+	
+	con.mainResourceServer = con.userPool.AddResourceServer(jsii.String("MainResourceServer"),
+		&awscognito.UserPoolResourceServerOptions{
+			Identifier: jsii.String("main"),
+			Scopes:     &[]awscognito.ResourceServerScope{adminScope},
+		})
+	
+	awsparams.Store(scope, "UserPoolIDParam", paramsNamespace, "user-pool-id", con.userPoolID)
+	awsparams.Store(scope, "UserPoolDomainParam", paramsNamespace, "user-pool-domain", jsii.Sprintf("%s-auth", qual))
+	
 	return con
 }
 
 func (i *identity) UserPool() awscognito.IUserPool {
 	return i.userPool
+}
+
+func (i *identity) UserPoolID() *string {
+	return i.userPoolID
+}
+
+func (i *identity) MainResourceServer() awscognito.IUserPoolResourceServer {
+	return i.mainResourceServer
+}
+
+func (i *identity) CognitoDomain() *string {
+	return i.cognitoDomain
 }
